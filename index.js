9e6125a812f2b760e7545744b231f9101418d80a
farhanmaneri <farhanmaneri@gmail.com>
2023-07-26 11:09:36 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// let catagories = require('./categories.json');
-let products = require('./product.json');
-// console.log(catagories);
-console.log(products);
+let catagories = require('./categories.json');
+// let products = require('./product.json');
+console.log(catagories);
+// console.log(products);
