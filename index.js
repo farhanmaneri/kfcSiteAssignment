d95ffa16f3366cde64309a9e5fabdfa24119965c
farhanmaneri <farhanmaneri@gmail.com>
2023-08-01 10:48:57 +0500

    practice forEach and filter

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let catagories = require('./categories.json');
-// let products = require('./product.json');
-console.log(catagories);
+// let catagories = require('./categories.json');
+let products = require('./product.json');
+// console.log(catagories);
 // console.log(products);
+
+// for (let i = 0; i< products.length; i++){
+//    if(products[i].price <200){
+//     console.log(products[i].price)
+//    }
+// } 
+
+// products.forEach(x => x.price < 200 ? console.log(x.price):  x.price);    
+
+// products.filter(x => x.price<200 ? console.log(x.price): x.price)
